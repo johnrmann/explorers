3d8d66a7bb8ef9353a9153b763c77875edd25bdb
johnrmann <accounts@jmann.io>
2024-11-02 15:17:22 -0700

    Add direction rotation utils.

--- src/math/direction.py
+++ src/math/direction.py
@@ -32,6 +32,17 @@ OPPOSITES = {
 	Direction.NORTHWEST: Direction.SOUTHEAST,
 }
 
+DIRECTIONS_CLOCKWISE_ORDER = [
+	Direction.NORTH,
+	Direction.NORTHEAST,
+	Direction.EAST,
+	Direction.SOUTHEAST,
+	Direction.SOUTH,
+	Direction.SOUTHWEST,
+	Direction.WEST,
+	Direction.NORTHWEST
+]
+
 DEFAULT_CAMERA_ORIENTATION = Direction.NORTHWEST
 
 def direction_opposite(direction: Direction):
@@ -39,6 +50,31 @@ def direction_opposite(direction: Direction):
 	Return the opposite of a given direction.
 	"""
 	return OPPOSITES.get(direction)
+
+def direction_rotate_90(direction: Direction, quarter_turns = 1):
+	"""
+	Rotates direction clockwise.
+
+	North --> East
+	West --> North
+	Northeast --> Southeast
+	"""
+	if quarter_turns == 0:
+		return direction
+	base = 0 if not is_direction_diagonal(direction) else 4
+	new_val = (((direction.value - base) + quarter_turns) % 4) + base
+	return Direction(new_val)
+
+def direction_rotate_45(direction: Direction, eighth_turns = 1):
+	"""
+	Rotates direction clockwise.
+	
+	North --> Northeast
+	Northwest --> North
+	"""
+	current = DIRECTIONS_CLOCKWISE_ORDER.index(direction)
+	new_idx = (current + eighth_turns) % len(DIRECTIONS_CLOCKWISE_ORDER)
+	return DIRECTIONS_CLOCKWISE_ORDER[new_idx]
 
 def direction_to_delta(direction: Direction):
 	"""
@@ -76,20 +112,26 @@ def left_wall_direction(orientation: Direction):
 	of the cell is SE. The left wall is therefore facing south and the right
 	wall is facing east.
 	"""
-	if orientation == Direction.NORTHWEST:
-		return Direction.SOUTH
-	pass
+	return direction_rotate_45(orientation, eighth_turns=5)
 
 def right_wall_direction(orientation: Direction):
 	"""
 	Read the docs for left_wall_direction.
 	"""
-	if orientation == Direction.NORTHWEST:
-		return Direction.EAST
-	pass
+	return direction_rotate_45(orientation, eighth_turns=3)
 
 def left_ridge_direction(orientation: Direction):
+	"""
+	We call lines drawn between tiles of different height to make the height
+	diff apparent "ridges."
+
+	If the camera is facing Northwest, the left ridge should be opposite of
+	the right wall.
+	"""
 	return direction_opposite(right_wall_direction(orientation))
 
 def right_ridge_direction(orientation: Direction):
+	"""
+	Read the docs for right_ridge_direction.
+	"""
 	return direction_opposite(left_wall_direction(orientation))
--- test/math/direction_test.py
+++ test/math/direction_test.py
@@ -1,9 +1,13 @@
+"""Tests for the direction util file."""
+
 import unittest
 
 from src.math.direction import *
 
 class DirectionTest(unittest.TestCase):
+	"""Tests for the direction util file."""
 	def test__is_direction_diagonal(self):
+		"""Test if directions are cardinal or diagonal."""
 		self.assertFalse(is_direction_diagonal(Direction.NORTH))
 		self.assertFalse(is_direction_diagonal(Direction.SOUTH))
 		self.assertFalse(is_direction_diagonal(Direction.EAST))
@@ -12,6 +16,123 @@ class DirectionTest(unittest.TestCase):
 		self.assertTrue(is_direction_diagonal(Direction.NORTHWEST))
 		self.assertTrue(is_direction_diagonal(Direction.SOUTHEAST))
 		self.assertTrue(is_direction_diagonal(Direction.SOUTHWEST))
+
+	def test__direction_rotate_90(self):
+		"""Test that it can handle rotating 90deg."""
+		self.assertEqual(
+			direction_rotate_90(Direction.NORTH, quarter_turns=1),
+			Direction.EAST,
+		)
+		self.assertEqual(
+			direction_rotate_90(Direction.NORTHEAST, quarter_turns=2),
+			Direction.SOUTHWEST,
+		)
+
+	def test__direction_rotate_90__loop(self):
+		"""Test that it can handle overflow."""
+		self.assertEqual(
+			direction_rotate_90(Direction.WEST, quarter_turns=1),
+			Direction.NORTH,
+		)
+		self.assertEqual(
+			direction_rotate_90(Direction.NORTHWEST, quarter_turns=2),
+			Direction.SOUTHEAST,
+		)
+
+	def test__direction_rotate_90__backward(self):
+		"""Test that it can handle negative numbers."""
+		self.assertEqual(
+			direction_rotate_90(Direction.SOUTH, quarter_turns=-1),
+			Direction.EAST
+		)
+		self.assertEqual(
+			direction_rotate_90(Direction.NORTHWEST, quarter_turns=-2),
+			Direction.SOUTHEAST
+		)
+
+	def test__direction_rotate_45__quarter(self):
+		"""Test quarter-turns expressed as 45deg increments."""
+		self.assertEqual(
+			direction_rotate_45(Direction.NORTH, eighth_turns=2),
+			Direction.EAST,
+		)
+		self.assertEqual(
+			direction_rotate_45(Direction.SOUTHEAST, eighth_turns=2),
+			Direction.SOUTHWEST,
+		)
+
+	def test__direction_rotate_45__eighth(self):
+		"""Tests eighth turns."""
+		self.assertEqual(
+			direction_rotate_45(Direction.NORTH, eighth_turns=1),
+			Direction.NORTHEAST,
+		)
+		self.assertEqual(
+			direction_rotate_45(Direction.SOUTHEAST, eighth_turns=3),
+			Direction.WEST,
+		)
+
+	def test__direction_rotate_45__loop(self):
+		"""Tests overflowing rotating directions by 45deg."""
+		self.assertEqual(
+			direction_rotate_45(Direction.NORTHWEST, eighth_turns=3),
+			Direction.EAST,
+		)
+		self.assertEqual(
+			direction_rotate_45(Direction.WEST, eighth_turns=3),
+			Direction.NORTHEAST,
+		)
+
+	def test__direction_rotate_45__backward(self):
+		"""Tests rotating directions by 45deg increments backward."""
+		self.assertEqual(
+			direction_rotate_45(Direction.NORTHWEST, eighth_turns=-3),
+			Direction.SOUTH,
+		)
+		self.assertEqual(
+			direction_rotate_45(Direction.WEST, eighth_turns=-3),
+			Direction.SOUTHEAST,
+		)
+
+	def test__left_wall_direction(self):
+		"""
+		The default camera orientation is NW, so the left wall should
+		be south.
+		"""
+		self.assertEqual(
+			left_wall_direction(Direction.NORTHWEST),
+			Direction.SOUTH
+		)
+
+	def test__right_wall_direction(self):
+		"""
+		The default camera orientation is NW, so the left wall should
+		be east.
+		"""
+		self.assertEqual(
+			right_wall_direction(Direction.NORTHWEST),
+			Direction.EAST
+		)
+
+	def test__left_ridge_direction(self):
+		"""
+		The default camera orientation is NW, so the left ridge should be
+		west.
+		"""
+		self.assertEqual(
+			left_ridge_direction(Direction.NORTHWEST),
+			Direction.WEST,
+		)
+
+	def test__right_ridge_direction(self):
+		"""
+		The default camera orientation is NW, so the right ridge should be
+		north.
+		"""
+		self.assertEqual(
+			right_ridge_direction(Direction.NORTHWEST),
+			Direction.NORTH,
+		)
 
 if __name__ == "__main__":
 	unittest.main()
