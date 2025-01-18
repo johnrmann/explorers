808b50c87b8f8fd521d76dccc3ed0efe15a309d6
johnrmann <accounts@jmann.io>
2025-01-18 18:13:50 -0500

    Merge jm/matrix-nonzero-dist into master.

--- src/math/matrix.py
+++ src/math/matrix.py
@@ -2,7 +2,23 @@
 Utility functions for smoothing matrixes out.
 """
 
+import math
+
 from src.math.adj import adj_cells
+
+def matrix_sized_as(
+		matrix: list[list[int]],
+		default: int = 0
+) -> list[list[int]]:
+	"""
+	Returns a matrix of the same size as the given matrix, filled with the
+	given default value.
+	"""
+	return [
+		[default for _ in row]
+		for row in matrix
+	]
+
 
 def smooth_matrix(
 	matrix,
@@ -75,3 +91,50 @@ def matrix_minfold_width(matrix):
 		for x in range(w // 2):
 			new_matrix[y][x] = min(matrix[y][x], matrix[y][x + w // 2])
 	return new_matrix
+
+
+def _distances_to_nonzero_values(matrix: list[list[int]]):
+	"""Helper for distances_to_nonzero_values."""
+	width = len(matrix[0])
+	height = len(matrix)
+
+	seen = matrix_sized_as(matrix, default=math.inf)
+
+	for y in range(height):
+		for x in range(width):
+			if matrix[y][x] != 0:
+				seen[y][x] = 0
+			else:
+				if x > 0:
+					seen[y][x] = min(seen[y][x], seen[y][x - 1] + 1)
+				if y > 0:
+					seen[y][x] = min(seen[y][x], seen[y - 1][x] + 1)
+
+	for y in range(height - 1, -1, -1):
+		for x in range(width - 1, -1, -1):
+			if matrix[y][x] != 0:
+				seen[y][x] = 0
+			else:
+				if x < width - 1:
+					seen[y][x] = min(seen[y][x], seen[y][x + 1] + 1)
+				if y < height - 1:
+					seen[y][x] = min(seen[y][x], seen[y + 1][x] + 1)
+
+	return seen
+
+
+def distances_to_nonzero_values(
+		matrix: list[list[int]],
+		loop_x = True,
+):
+	"""
+	Cells in matrix that are nonzero will be replaced with zero, and zero
+	cells will be replaced by the distance to the nearest nonzero cell in the
+	original matrix.
+	"""
+	if not loop_x:
+		return _distances_to_nonzero_values(matrix)
+	else:
+		doubled = matrix_double_width(matrix)
+		distances = _distances_to_nonzero_values(doubled)
+		return matrix_minfold_width(distances)
--- test/math/matrix_test.py
+++ test/math/matrix_test.py
@@ -2,10 +2,12 @@ import unittest
 
 from src.math.adj import adj_cells
 from src.math.matrix import (
+	matrix_sized_as,
 	smooth_matrix,
 	round_matrix_to_int,
 	matrix_double_width,
 	matrix_minfold_width,
+	distances_to_nonzero_values,
 )
 
 TEST_MAT = [
@@ -27,6 +29,15 @@ TEST_FLOAT_MAT = [
 ]
 
 class SmoothMatrixTest(unittest.TestCase):
+	def test__matrix_sized_as__basic(self):
+		result = matrix_sized_as(TEST_MAT, default=0)
+		self.assertEqual(len(result), 3)
+		self.assertEqual(len(result[0]), 3)
+		for y in range(3):
+			for x in range(3):
+				self.assertEqual(result[y][x], 0)
+
+
 	def test__sanity__correct_adj(self):
 		d = (3,3)
 		self.assertEqual(
@@ -100,6 +111,62 @@ class SmoothMatrixTest(unittest.TestCase):
 			for x in range(3):
 				self.assertEqual(result[y][x], 0)
 
+
+	def test__distances_to_nonzero_values__basic(self):
+		oasis = [
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+			[0, 0, 1, 0, 0],
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+		]
+		expected = [
+			[4, 3, 2, 3, 4],
+			[3, 2, 1, 2, 3],
+			[2, 1, 0, 1, 2],
+			[3, 2, 1, 2, 3],
+			[4, 3, 2, 3, 4],
+		]
+		result = distances_to_nonzero_values(oasis)
+		self.assertEqual(result, expected)
+
+
+	def test__distances_to_nonzero_values__edge_unlooped(self):
+		oasis = [
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+			[1, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+		]
+		expected = [
+			[2, 3, 4, 5, 6],
+			[1, 2, 3, 4, 5],
+			[0, 1, 2, 3, 4],
+			[1, 2, 3, 4, 5],
+			[2, 3, 4, 5, 6],
+		]
+		result = distances_to_nonzero_values(oasis, loop_x=False)
+		self.assertEqual(result, expected)
+
+
+	def test__distances_to_nonzero_values__edge_looped(self):
+		oasis = [
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+			[1, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+		]
+		expected = [
+			[2, 3, 4, 4, 3],
+			[1, 2, 3, 3, 2],
+			[0, 1, 2, 2, 1],
+			[1, 2, 3, 3, 2],
+			[2, 3, 4, 4, 3],
+		]
+		result = distances_to_nonzero_values(oasis, loop_x=True)
+		self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
